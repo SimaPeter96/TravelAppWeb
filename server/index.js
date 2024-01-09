3749e51356be20eb3e913e5743a62ca9587f813f
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2024-01-09 15:14:54 +0200

    modified .env file and index.js folder

--- server/index.js
+++ server/index.js
@@ -1,3 +1,25 @@
-const express = require('expresss')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
+require("dotenv").config()
+
+app.use(express.json());
+app.use(cors());
+
+CRUD 
+
+app.get("/", (req, res) )
+
+const port = process.env.PORT || 5001; 
+const uri = process.env.ATLAS_URI; 
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=> console.log("MongoDB connection established")).catch((error) => console.log("MongoDB connection failed: ", error.message))
