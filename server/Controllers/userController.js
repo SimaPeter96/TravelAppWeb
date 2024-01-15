408666aa754ce4a64fc7e3a69813a371581afece
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2024-01-15 13:30:39 +0200

    installed bcrypt packages , validator packagers and jasonwebtoken packages

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,1 +1,10 @@
-const userModel = require("../Models/userModel")
+const userModel = require("../Models/userModel");
+const bcrypt = require("bcrypt");
+const validator = require ("validator");
+const jwt =  require("jsonwebtoken");
+
+const registerUser = (req, res) => {
+    res.send("Register...");
+};
+
+module.exports = { registerUser };
--- server/Routes/userRouter.js
+++ server/Routes/userRouter.js
@@ -1,9 +1,10 @@
-const express = require("express")
+const express = require("express");
+const {registerUser} = require("../Controllers/userController");
+
+
 
 const router = express.Router();
 
-router.get("/register", (req, res) =>{
-    res.send("Register");
-});
+router.get("/register", registerUser);
 
 module.exports = router;
