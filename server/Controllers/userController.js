c2ce892352e0a271d0b2cf8b4386b4f4c1cd2b13
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2024-01-18 11:02:54 +0200

    modified  UserController.js

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -62,7 +62,36 @@ const loginUser = async (req, res) => {
         const token = createToken(user._id);
     
         res.status(200).json({_id: user._id, name: user.name, email, token });
-  } catch(error){}
+  } catch(error){
+    console.log(error)
+    res.status(500).json(error);
+  }
 };
 
-module.exports = { registerUser, loginUser };
+const findUser = async(req, res) => {
+    const userId = req.params.userId;
+    try{
+        const user = await userModel.findById(userId);
+
+        res.status(200).json(user);
+    }catch(error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
+
+const getUsers = async(req, res) => {
+    
+    try{
+        const user = await userModel.find(userId);
+
+        res.status(200).json(user);
+    }catch(error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
+
+
+
+module.exports = { registerUser, loginUser, findUser, getUsers};
--- server/Routes/userRouter.js
+++ server/Routes/userRouter.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const {registerUser, loginUser} = require("../Controllers/userController");
+const {registerUser, loginUser, findUser} = require("../Controllers/userController");
 
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/find/:userId", findUser);
+
 
 
 module.exports = router;
