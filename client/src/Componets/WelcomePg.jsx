62400726adb430f45ec1d4b8868e65953149b450
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2024-01-30 12:21:20 +0200

    added a navbar and logo to the Welcom Page

--- client/src/Componets/WelcomePg.jsx
+++ client/src/Componets/WelcomePg.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Photo from '../Componets/Images/welcomeBg.jpg';
+import Logo from '../Componets/Images/TravelAppLogo.png';
+import 'tailwindcss/tailwind.css';
 
 function WelcomePg() {
   return (
-    <div>WelcomePg</div>
+    <div className="relative min-h-screen flex items-center justify-center">
+        <img src={Photo} alt="Photo" className='relative w-full h-full object-cover' />
+        <nav className="bg-transparent p-4 absolute top-0 left-0 right-0  bg-white bg-opacity-30 shadow-md">
+        <img src={Logo} className="logo" alt='logo' style={{ width: '80px' }} />
+      </nav>
+
+    </div>
   )
 }
 
